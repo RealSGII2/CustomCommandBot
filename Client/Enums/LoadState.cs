0d20c7c4d1da521835de243129a63ea94c626b2a
Thomas Wise <thomas@realsgii2.dev>
2021-07-27 17:28:53 -0700

    Add loadstate & load guild

---
+++ Client/Enums/LoadState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomCommandBot.Client.Enums
+{
+    public enum LoadState
+    {
+        Unfinished,
+        Error,
+        Ok
+    }
+}
